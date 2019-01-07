38cdc186722b9bf38b3802ea2fe289fc030336c3
jlrios41 <Jlrios@nevada.unr.edu>
2019-01-07 15:39:01 -0800

    Added bootstrap and added classes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //create selectors
 let btnEnter = document.querySelector('button');
 let list = document.querySelector('ul');
+list.setAttribute('class','list-group list-group');
 let input = document.querySelector('input');
 
 
@@ -8,19 +9,26 @@ let input = document.querySelector('input');
 function addItem(){
     //create new list item
     let newLi=document.createElement('li');
+    newLi.setAttribute('class','list-group-item');
     //get input value
     let item=input.value;
     //add input to list item
     newLi.textContent=item;
     //clear input field
     input.value='';
+    //create check button
+    let chkBtn = document.createElement('button');
     //create delete button
     let delBtn = document.createElement('button');
-    //set delete class for each button
-    delBtn.setAttribute('class','del');
-    //add text to button
-    delBtn.innerHTML='Delete';
-    //add delete button to list item
+    //set strikethrough class for check button
+    chkBtn.setAttribute('class', 'btn-outline-success btn-sm')
+    //set delete class for delete button
+    delBtn.setAttribute('class','del btn-outline-danger btn-sm');
+    //add text to buttons
+    chkBtn.innerHTML=' &#10003;';
+    delBtn.innerHTML='X';
+    //add buttons to list item
+    newLi.appendChild(chkBtn);
     newLi.appendChild(delBtn);
     //add list item to list
     list.appendChild(newLi);
@@ -33,9 +41,13 @@ btnEnter.addEventListener('click', addItem);
 //handle click event for when delete button is clicked 
 list.addEventListener('click', function(e){
         //check that the button is being clicked
-        if(e.target.className=='del'){
+        if(e.target.className=='del btn-outline-danger btn-sm'){
             const delLi=e.target.parentElement;
             list.removeChild(delLi);
+        }
+        if(e.target.className=='btn-outline-success btn-sm'){
+            const chkLi=e.target.parentElement;
+            chkLi.setAttribute('class','list-group-item chk');
         }
 })
 
